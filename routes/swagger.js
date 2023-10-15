a8946cbbe0ddc6392680ae76883392a67226f2b0
espnal <78657757+espnal@users.noreply.github.com>
2023-10-15 18:23:22 -0400

    latest

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express')
+
 const swaggerDocuments = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocuments));
--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title:'My API Roguin for Vehicles',
         description:'Vehicles API'
     },
-    host: 'localhost:8080',
-    schemes:['http']
+    host: 'mongodb-rest-api.onrender.com',
+    schemes:['https']
 }; 
 
 const outFile = './swagger.json';
