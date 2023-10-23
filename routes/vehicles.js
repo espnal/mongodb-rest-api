4b0c16b33384e6173ce36076c20c7875e83bb7b6
espnal <78657757+espnal@users.noreply.github.com>
2023-10-23 08:40:27 -0400

    ....

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const vehiclesController = require('../controllers/vehicles');
 const validation = require('../middleware/validate')
-
+// validation.saveVehicles
 router.get('/', vehiclesController.getAll);
-router.get('/:id', validation.saveVehicles, vehiclesController.getSingle);
+router.get('/:id', vehiclesController.getSingle);
 router.post('/', vehiclesController.createVehicle);
-router.put('/:id', validation.saveVehicles, vehiclesController.updateVehicle);
-router.delete('/:id', validation.saveVehicles, vehiclesController.deleteVehicle);
+router.put('/:id',  vehiclesController.updateVehicle);
+router.delete('/:id', vehiclesController.deleteVehicle);
 
 module.exports = router;
