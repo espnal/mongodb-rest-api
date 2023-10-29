08fa4c92d5ccaee4588fb3d3c7bfd30be52d40ac
espnal <78657757+espnal@users.noreply.github.com>
2023-10-28 22:34:08 -0400

    latest

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/vehicles', require('./vehicles'))
+router.use('/user', require('./user'))
 
 module.exports = router;
