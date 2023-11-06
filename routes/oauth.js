ff5de09fc0814bc6952f129fa967a61c0f64144d
espnal <78657757+espnal@users.noreply.github.com>
2023-11-05 22:38:38 -0400

    lastest

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const oauthController = require('../controllers/oauthController');
 
-router.get('/auth', oauthController.redirectToGitHub);
+router.get('/oauth', oauthController.redirectToGitHub);
 router.get('/oauth-callback', oauthController.handleGitHubCallback);
 
 module.exports = router;
